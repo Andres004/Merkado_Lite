1d4de03618ff714d6a283da363eeb5886be0be84
Ariana Pita <ariana.pita@ucb.edu.bo>
2025-11-12 12:24:13 -0400

    Se añadio la seccion categorias

--- mklite_frontend/app/page.tsx
+++ mklite_frontend/app/page.tsx
@@ -3,12 +3,14 @@
 import { features } from 'process';
 import OfferBanner from './components/OfferBanner'; 
 import ServicesSection from './components/ServicesSection';
+import CategoriesSection from './components/CategoriesSection';
 
 export default function HomePage() {
   return (
     <main>
       <OfferBanner />
       <ServicesSection />
+      <CategoriesSection />
       {/* ⬅️ Aquí irán los demás componentes: Servicios, Categorías, Productos... */}
     </main>
   );
