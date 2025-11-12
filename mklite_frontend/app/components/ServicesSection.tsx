5c54420fc963ba04908b8835db006c9d2949b65b
Ariana Pita <ariana.pita@ucb.edu.bo>
2025-11-12 12:22:03 -0400

    Cambio de titulo de contenedor a negro

--- mklite_frontend/app/components/ServicesSection.tsx
+++ mklite_frontend/app/components/ServicesSection.tsx
@@ -45,7 +45,7 @@ const ServicesSection = () => {
 
               {/* Contenido del Servicio */}
               <div>
-                <h3 className="text-base font-semibold mb-1">
+                <h3 className="text-base font-semibold mb-1 text-black">
                   {service.title}
                 </h3>
                 <p className="text-sm text-gray-500">
