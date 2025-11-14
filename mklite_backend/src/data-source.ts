8b6cf34f87999c9ffd4db5cd1dd0012dac060d53
dahbner <dabnerorozco123@gmail.com>
2025-11-13 21:50:50 -0400

    Creando la base en postgres

--- mklite_backend/src/data-source.ts
+++ mklite_backend/src/data-source.ts
@@ -2,13 +2,13 @@ import { DataSource } from "typeorm";
 import { User } from "./entity/user.entity";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "postgres",
     host: "localhost",
-    port: 3306,
+    port: 5432,
     username: "merkado_admin",
     password: "123456789",
     database: "mklite",
-    synchronize: true,
+    synchronize: false,
     logging: true,
     entities: [User],
     subscribers: [],
