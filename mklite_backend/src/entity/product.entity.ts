f8608a88bd589bc835344d5dcd058147e405c354
KathM00 <kathymontamejia@gmail.com>
2025-11-16 19:33:33 -0400

    Arreglos

--- mklite_backend/src/entity/product.entity.ts
+++ mklite_backend/src/entity/product.entity.ts
@@ -1,23 +1,35 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { CartItem } from './cartitem.entity';
+import { Batch } from './batch.entity';
+import { Inventory } from './inventory.entity';
+import { StockAlert } from './stockalert.entity';
 
-@Entity('producto') 
+@Entity('producto')
 export class Product {
-    @PrimaryGeneratedColumn({ name: 'id_producto' }) 
-    id_producto: number;
+  @PrimaryGeneratedColumn({ name: 'id_producto' })
+  id_producto: number;
 
-    @Column({ length: 150 })
-    nombre: string;
+  @Column({ length: 150 })
+  nombre: string;
 
-    @Column({ type: 'text' })
-    descripcion: string;
+  @Column({ type: 'text' })
+  descripcion: string;
 
-    @Column({ type: 'decimal', precision: 10, scale: 2 })
-    precio_venta: number;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  precio_venta: number;
 
-    @Column({ length: 255 })
-    imagen_url: string;
+  @Column({ length: 255 })
+  imagen_url: string;
 
-    @OneToMany(() => CartItem, (cartItem) => cartItem.product)
-  cartItems: CartItem[];
+  //@OneToMany(() => CartItem, (cartItem) => cartItem.producto)
+  //cartItems: CartItem[];
+
+  @OneToMany(() => Batch, (batch) => batch.producto)
+  lotes: Batch[];
+
+  @OneToMany(() => Inventory, (inv) => inv.producto)
+  inventario: Inventory[];
+
+  @OneToMany(() => StockAlert, (alerta) => alerta.producto)
+  alertas: StockAlert[];
 }
--- mklite_backend/src/product/product.module.ts
+++ mklite_backend/src/product/product.module.ts
@@ -1,11 +1,20 @@
-import { Module } from "@nestjs/common";
-import { ProductService } from "./product.service";
-import { ProductController } from "./product.controller";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Product } from '../entity/product.entity';
+import { Batch } from '../entity/batch.entity';
+import { Inventory } from '../entity/inventory.entity';
+import { StockAlert } from '../entity/stockalert.entity';
+
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Product, Batch, Inventory, StockAlert]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
-  exports: [ProductService], 
+  exports: [ProductService],
 })
 export class ProductModule {}
