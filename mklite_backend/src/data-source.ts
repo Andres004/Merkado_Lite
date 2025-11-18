01810e78d5d80c68765b7bb67286845e6e5f2198
MateinSpy <quintanillacalvimateo@gmail.com>
2025-11-18 02:14:40 -0400

    Configuracion inicial lista y base de datos conectada

--- mklite_backend/src/data-source.ts
+++ mklite_backend/src/data-source.ts
@@ -1,20 +1,28 @@
 import { DataSource } from "typeorm";
-import { join } from 'path';
+import { join } from "path"; // Importante para arreglar rutas
+
+// 1. IMPORTAMOS MANUALMENTE LAS ENTIDADES CONFLICTIVAS
+import { User } from "./entity/user.entity";
+import { Role } from "./entity/role.entity";
+import { UserRole } from "./entity/userrole.entity";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "merkado_admin",
-<<<<<<< HEAD
-    password: "123",
-=======
-    password: "Merkado2025!", //Password local, no me dejo crear usuario con password 123
->>>>>>> 41d5472282f84790036fd5dd0821eff019cba2df
-    database: "mklite",
-    synchronize: true,
-    logging: true,
-    entities: [join(__dirname, '**', '*.entity{.js,.ts}')],
-    subscribers: [],
-    migrations: [],
-})
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "merkado_admin",
+  password: "123",
+  database: "mklite",
+  synchronize: true,
+  logging: true,
+  entities: [
+    // 2. LAS PONEMOS PRIMERO EN LA LISTA
+    User, 
+    Role, 
+    UserRole, 
+    // 3. LUEGO CARGAMOS EL RESTO AUTOMÁTICAMENTE
+    join(__dirname, "**", "*.entity.{ts,js}"),
+  ],
+  subscribers: [],
+  migrations: [],
+});
