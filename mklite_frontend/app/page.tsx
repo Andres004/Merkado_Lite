b441ce8d50cf1521bd2856024017650921d85784
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-11-25 01:34:40 -0400

    cargar datos desde la BD

--- mklite_frontend/app/page.tsx
+++ mklite_frontend/app/page.tsx
@@ -1,20 +1,42 @@
-// mklite_frontend/app/page.tsx
+"use client"; 
 
-import { features } from 'process';
+import React, { useEffect, useState } from 'react';
 import OfferBanner from './components/OfferBanner'; 
 import ServicesSection from './components/ServicesSection';
 import CategoriesSection from './components/CategoriesSection';
 import ProductGrid from './components/ProductGrid';
 
+// importacion el servicio y modelo
+import { getAllProducts } from './services/product.service';
+import { ProductModel } from './models/product.model';
+
 export default function HomePage() {
+  const [products, setProducts] = useState<ProductModel[]>([]);
+
+  useEffect(() => {
+    // Función para cargar datos reales
+    const fetchData = async () => {
+      const data = await getAllProducts();
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <main>
+    <main className="min-h-screen bg-gray-50">
       <OfferBanner />
       <ServicesSection />
       <CategoriesSection />
-      <ProductGrid title="Novedades" />
-      <ProductGrid title="Más Vendidos" />
-      {/* ⬅️ Aquí irán los demás componentes: Servicios, Categorías, Productos... */}
+
+      <ProductGrid 
+        title="Novedades" 
+        products={products.slice(0, 6)} 
+      />
+      
+      <ProductGrid 
+        title="Más Vendidos" 
+        products={products.slice(4, 8)} 
+      />
     </main>
   );
-} 
+}
