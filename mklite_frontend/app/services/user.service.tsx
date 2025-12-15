dcfc0d34aef02d5b54ae83df600a53158fd24a46
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-12-15 09:03:59 -0400

    agregando el estado de cuenta de user en model

--- mklite_frontend/app/services/user.service.tsx
+++ mklite_frontend/app/services/user.service.tsx
@@ -1,30 +1,35 @@
-import UserModel from "../models/user.model";
-import { instance } from "../utils/axios"
+import { CreateUserDto, PaginatedUsersResponse, UpdateUserDto, UserModel } from "../models/user.model";
+import { instance } from "../utils/axios";
 
-export const getUsers = async () => {
-    const users = await instance.get('/user');
+export const getUsers = async (params?: { page?: number; limit?: number; search?: string }) => {
+    const users = await instance.get<PaginatedUsersResponse>('/user', { params });
     return users.data;
-}
+};
 
-export const createUser = async (user: Partial<UserModel>) => {
-    const newUser = await instance.post('/user', user);
+export const createUser = async (user: CreateUserDto) => {
+    const newUser = await instance.post<UserModel>('/user', user);
     return newUser.data;
-}
+};
 
-export const deleteUser = async (ci: string) => {
-    const deletedUser = await instance.delete(`/user/${ci}`);
+export const deleteUser = async (id_usuario: number) => {
+    const deletedUser = await instance.delete(`/user/${id_usuario}`);
     return deletedUser.data;
-}
+};
 
-export const updateUser = async (ci: string, user: any) => {
-    const updatedUser = await instance.put(`/user/${ci}`, user);
+export const updateUser = async (id_usuario: number, user: UpdateUserDto) => {
+    const updatedUser = await instance.put<UserModel>(`/user/${id_usuario}`, user);
     return updatedUser.data;
-}
+};
+
+export const updateUserStatus = async (id_usuario: number, estado_cuenta: string) => {
+    const response = await instance.patch<UserModel>(`/user/${id_usuario}/status`, { estado_cuenta });
+    return response.data;
+};
 
 export const getUserById = async (id_usuario: number) => {
-  const response = await instance.get(`/user/${id_usuario}`);
+  const response = await instance.get<UserModel>(`/user/${id_usuario}`);
   return response.data;
-}
+};
 
 export const changePassword = async (payload: {
   currentPassword: string;
