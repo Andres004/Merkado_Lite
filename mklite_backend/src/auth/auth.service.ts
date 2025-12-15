51d2fdd681ace3c0eb721c0efcb862e6836883c2
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2025-12-15 09:02:53 -0400

    agregando estado_cuenta en user

--- mklite_backend/src/auth/auth.service.ts
+++ mklite_backend/src/auth/auth.service.ts
@@ -10,64 +10,40 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  // 1. Validar usuario y contraseña
-  /*
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userService.findByEmailForAuth(email);
-    
-    if (user && user.password && await bcrypt.compare(pass, user.password)) {
-      const { password, ...result } = user;
-      return result; 
-    }
-    return null;
-  }
-   */
 
-  async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.userService.findByEmailForAuth(email);
-    
     if (user && user.password && await bcrypt.compare(pass, user.password)) {
+      if (user.accountStatus && user.accountStatus.toLowerCase() !== 'activo') {
+        throw new UnauthorizedException('La cuenta está inactiva');
+      }
+
       const { password, ...result } = user;
 
-      // Sacamos el rol principal desde la relación usuario_rol -> rol
       const mainRole =
-        user.userRoles?.[0]?.role?.nombre ?? 'CLIENTE'; // por si no tuviera nada
+        user.userRoles?.[0]?.role?.nombre ?? 'CLIENTE';
 
       return {
         ...result,
-        rol: mainRole, // <- AQUÍ se define el rol que verá el front
+        rol: mainRole,
       };
     }
     return null;
   }
 
-
-  // 2. Login (Directo, genera token inmediatamente)
   async login(user: any) {
     return this.generateAccessToken(user);
   }
 
-  // 3. Registro (Puente al servicio de usuarios)
   async register(userData: any) {
     return this.userService.registerUser(userData);
   }
 
-  // Auxiliar: Generar JWT
-  /*
   private generateAccessToken(user: any) {
-    const payload = { email: user.email, sub: user.id_usuario };
-    return {
-      access_token: this.jwtService.sign(payload),
-      user: user
-    };
-  }
-   */
-
-  private generateAccessToken(user: any) {
-  const payload = { 
-    email: user.email, 
+  const payload = {
+    email: user.email,
     sub: user.id_usuario,
-    rol: user.rol, // opcional, pero útil
+    rol: user.rol,
   };
 
   return {
