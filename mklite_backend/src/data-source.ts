0a192c60310dc0718a49d0973742d10ace0c3841
dahbner <dabnerorozco123@gmail.com>
2025-12-18 07:43:10 -0400

    data source

--- mklite_backend/src/data-source.ts
+++ mklite_backend/src/data-source.ts
@@ -4,17 +4,18 @@ import { join } from 'path';
 export const AppDataSource = new DataSource({
     type: "mysql",
     
-    //URL de Railway
+    // URL de Railway 
     url: process.env.DATABASE_URL,
 
-    // Variables de Railway
+    // Variables individuales (Fallbacks)
     host: process.env.MYSQLHOST || "localhost",
     port: Number(process.env.MYSQLPORT) || 3306,
     username: process.env.MYSQLUSER || "merkado_admin",
     password: process.env.MYSQLPASSWORD || "123",
     database: process.env.MYSQLDATABASE || "mklite",
 
-    synchronize: true,
+    synchronize: process.env.SYNCHRONIZE === 'true', 
+    
     logging: true,
     entities: [join(__dirname, '**', '*.entity{.js,.ts}')],
     subscribers: [],
